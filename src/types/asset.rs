462a0b1be087590336873252b2f529aeaf92d138
tiannian <dtiannian@gmail.com>
2025-06-03 20:28:45 +0800

    add debug for asset

--- src/types/asset.rs
+++ src/types/asset.rs
@@ -4,16 +4,19 @@ use alloy_primitives::Address;
 
 use super::NetworkId;
 
+#[derive(Debug)]
 pub enum AssetAddress {
     Eip155(Address),
     Native,
 }
 
+#[derive(Debug)]
 pub enum AssetType {
     Crypto,
     Fiat,
 }
 
+#[derive(Debug)]
 pub struct Asset {
     pub symbol: String,
     pub asset_id: String,
